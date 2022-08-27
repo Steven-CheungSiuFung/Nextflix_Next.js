df2fe04b6f68495184145856fb11109db8f57ee0
Steven Cheung <crsiufung@hotmail.com>
2022-08-27 17:22:57 +0800

    implement my-list function

--- pages/my-list/index.js
+++ pages/my-list/index.js
@@ -1,5 +1,53 @@
-const MyListPage = () => {
-  return <div>My List</div>;
+import { veriftToken } from "../../lib/auth";
+import { getFavouritedVideoId } from "../../lib/db/hasura";
+
+import NavBar from "../../components/nav-bar/nav-bar.component";
+import SectionCards from "../../components/section-cards/section-cards.component";
+
+import classes from "./index.module.css";
+
+const MyListPage = ({ favouritedVideos }) => {
+  return (
+    <div className={classes.container}>
+      <NavBar />
+
+      <div className={classes.videos}>
+        <SectionCards
+          title="Favourited"
+          size="large"
+          videos={favouritedVideos}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MyListPage;
+
+export const getServerSideProps = async (context) => {
+  const token = context.req.cookies.token;
+
+  const issuer = token ? veriftToken(token) : null;
+
+  if (!issuer) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  const response = await getFavouritedVideoId(token, issuer);
+  const videoIdArray = response.data.stats;
+  const favouritedVideos = videoIdArray.map((item) => ({
+    id: item.videoId,
+    imgUrl: `https://i.ytimg.com/vi/${item.videoId}/maxresdefault.jpg`,
+  }));
+
+  return {
+    props: {
+      favouritedVideos,
+    },
+  };
+};
